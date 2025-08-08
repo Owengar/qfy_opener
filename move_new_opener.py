c0208c3f062121c3604ab48ef8ea575db9833dea
Owengar <owengaalt@gmail.com>
2025-08-08 11:05:16 -0500

    fixes

--- move_new_opener.py
+++ move_new_opener.py
@@ -11,12 +11,18 @@ import shutil
 parent_pid = int(sys.argv[1])
 source_path = sys.argv[2]
 
-while psutil.pid_exists(parent_pid):
-    pass
+
 
 output = open(source_path + "\\mover_says_hi.txt", "w")
 sys.stdout = output
 sys.stderr = output
+
+
+while psutil.pid_exists(parent_pid):
+    pass
+print(parent_pid)
+print(source_path)
+psutil.pid_exists(parent_pid)
 
 shutil.copyfile(source_path + "\\new_opener.exe", source_path + "\\open_quantify.exe")
 subprocess.run(source_path + "\\open_quantify.exe", shell=True)

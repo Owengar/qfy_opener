83bdb5d1287bf36ac00f4b2f150c1bfe5895fbcd
Owengar <owengaalt@gmail.com>
2025-08-08 10:31:57 -0500

    new commit

---
+++ move_new_opener.py
@@ -0,0 +1,20 @@
+import sys
+sys.dont_write_bytecode = True
+
+import sys, os
+import psutil
+import subprocess
+import shutil
+
+
+
+parent_pid = int(sys.argv[1])
+source_path = sys.argv[2]
+
+while psutil.pid_exists(parent_pid):
+    pass
+
+
+
+shutil.copyfile(source_path + "\\new_opener.exe", source_path + "\\open_quantify.exe")
+subprocess.run(source_path + "\\open_quantify.exe", shell=True)
